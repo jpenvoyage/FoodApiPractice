6aa111cf7962b25ac500a3e296dc37787d3eb470
joaobug <joaoenfrance@gmail.com>
2024-02-01 23:16:17 -0300

    Complete App

--- FoodApiPractice/ContentView.swift
+++ FoodApiPractice/ContentView.swift
@@ -8,16 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var recipes: [Recipe] = []
+    let recipeClient = RecipeClient()
+    @State private var recipeName: String = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("Enter name", text: $recipeName)
+                .textFieldStyle(.roundedBorder)
+                .onSubmit {
+                    Task{
+                        do{
+                            recipes = try await recipeClient.searchRecipe(recipeName)
+                        } catch {
+                            print(error)
+                        }}
+                }.padding()
+            List(recipes) { recipe in
+                
+                HStack {
+                    AsyncImage(url: recipe.featuredImage) { image in
+                        image.resizable()
+                            .clipShape(RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/))
+                            .frame(width: 100, height: 100)
+                    } placeholder: {
+                    ProgressView("Loading")}
+                    
+                    Text(recipe.title)
+                }
+                
+                
+            }
         }
-        .padding()
-    }
-}
+        }    }
+
 
 #Preview {
     ContentView()
